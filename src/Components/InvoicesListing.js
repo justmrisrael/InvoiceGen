20e17e80ac247edd1b6ea4d9c0ce7b4a5057d008
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2022-01-14 11:24:48 -0600

    master

--- src/Components/InvoicesListing.js
+++ src/Components/InvoicesListing.js
@@ -1,30 +1,51 @@
 import React from "react";
 
 export default class InvoicesListing extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-    }
+    this.state = {
+      invoicesData: [],
+    };
+  }
 
-    componentDidMount() {
-        //auto runtime
-        fetch("api.readinvoice/all",{
-            method: "GET",
-        }).then((response) => {
-            if(response.ok){
-                //get was successful
-                console.log(response.json());
-            }else{
-                //get was not successful
-                console.log("Error reading invoice");
+  componentDidMount() {
+    //auto runtime
+    fetch("/api/readinvoice/all", {
+      method: "GET",
+    }).then((response) => {
+        if (response.ok) {
+          //get was successful
+          return response.json();
+          console.log(response.json());
+        } else {
+          //get was not successful
+          console.log("Error reading invoice");
+        }
+      }).then((responseAsJson) => {
+        let invoicesInfo = [];
+        responseAsJson.map((invoice, index) => {
+          invoicesInfo.push(
+              {
+                id: invoice._id,
+                description: invoice.invoiceDescription,
             }
-        })
-    }
+          );
+        });
+        this.setState((state, props)=>{
+            return {
+                invoicesData : state.invoicesData.concat(invoicesInfo)
+            }
+        });
 
-    render() {
-        return(
-            //placeholder
-            <h1>Hello from Invoices Listing</h1>
-        )
-    }
+        console.log(this.state.invoicesData);
+      });
+  }
+
+  render() {
+    return (
+      //placeholder
+      <h1>Hello from Invoices Listing</h1>
+    );
+  }
 }
